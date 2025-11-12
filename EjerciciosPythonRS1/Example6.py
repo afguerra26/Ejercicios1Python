7333243fc1f6c3691ce0f591a5181771f50c2191
afguerra26 <mafjr1994@gmail.com>
2025-11-12 13:51:58 -0500

    Delete EjerciciosPythonRS1 directory

--- EjerciciosPythonRS1/Example6.py
+++
@@ -1,9 +0,0 @@
-tiempo =float(input("ingrseel numero de horas de servicio: "))
-hora = 2000
-multa = 5000
-if tiempo <= 4.9 :
-    total = tiempo * hora
-    print(f"el valor a pagar es de: {total}")
-elif tiempo >=5 :
-    total = (tiempo * hora) + multa
-    print(f"el valor a pagar es de: {total}")    
